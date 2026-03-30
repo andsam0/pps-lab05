ac6fa760adb879a05915fde04e859261a3a46a5d
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-03-30 10:13:04 +0200

    Vector2DImpl

--- src/main/scala/it/unibo/pps/ex/Vector2D.scala
+++ src/main/scala/it/unibo/pps/ex/Vector2D.scala
@@ -25,13 +25,25 @@ trait Vector2D:
 
 object Vector2D:
   // Factory method to create Vector2D instances
-  def apply(x: Double, y: Double): Vector2D = ???
+  def apply(x: Double, y: Double): Vector2D = Vector2DImpl(x, y)
 
   // Common vectors (optional but nice)
   val zero: Vector2D = apply(0.0, 0.0)
   val i: Vector2D = apply(1.0, 0.0) // Unit vector along x-axis
   val j: Vector2D = apply(0.0, 1.0) // Unit vector along y-axis
 
+  private case class Vector2DImpl(override val x: Double, override val y: Double) extends Vector2D {
+
+    override def +(other: Vector2D): Vector2D = Vector2D(this.x + other.x, this.y + other.y)
+
+    override def -(other: Vector2D): Vector2D = Vector2D(this.x - other.x, this.y - other.y)
+
+    override def *(scalar: Double): Vector2D = Vector2D(this.x * scalar, this.y * scalar)
+
+    override def dot(other: Vector2D): Double = this.x * other.x + this.y * other.y
+
+    override def magnitude: Double = sqrt(this.x * this.x + this.y * this.y)
+  }
 
 /** Hints:
  *   - Implement Vector2D with a Vector2DImpl class.
