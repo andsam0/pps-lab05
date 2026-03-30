889d32e2c1c012c4ebd150184dca70c8416ae6ac
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-03-30 11:40:26 +0200

    started LogicImpl

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -4,8 +4,12 @@ import it.unibo.pps.polyglot.a05b.Logics
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
+  private val random = scala.util.Random(42)
+  private var tickCount = 0
+  private val initial: (Int, Int) = (random.nextInt(size-2)+1,random.nextInt(size-2)+1)
+
+  override def tick(): Unit = tickCount += 1
 
   override def isOver: Boolean = false
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def hasElement(x: Int, y: Int): Boolean = true
